f958d1c09edaf79f7f697636e15177afdf886fd3
Sergio Palacio Rodenas <sergiopalaciobach@gmail.com>
2022-12-28 19:25:20 +0100

    Feature/Add Passport

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::middleware('client')->get('/user', function (Request $request) {
+    return [
+        'message' => 'loaded',
+        'data' =>  $request->user()
+    ];
 });
